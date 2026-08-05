4d9f4b180ec707b63398e617f0668f47a82aecbd
Tiny Block CI <ci@nosuchgames.com>
2026-08-05 12:20:20 +0700

    Deploy Tiny Block web 82920c6

---
+++ play/game/Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version:5.9
+import PackageDescription
+
+let package = Package(
+    name: "PoingGodotAdMobDeps",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(
+            name: "PoingGodotAdMobDeps",
+            targets: ["PoingGodotAdMobDeps"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", exact: "13.7.0"),
+        .package(url: "https://github.com/googleads/swift-package-manager-google-user-messaging-platform.git", exact: "3.1.0"),
+        .package(url: "https://github.com/googleads/googleads-mobile-ios-mediation-mintegral.git", exact: "8.1.500"),
+        .package(url: "https://github.com/googleads/googleads-mobile-ios-mediation-liftoffmonetize.git", exact: "7.7.400"),
+    ],
+    targets: [
+        .target(
+            name: "PoingGodotAdMobDeps",
+            dependencies: [
+                .product(name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads"),
+                .product(name: "GoogleUserMessagingPlatform", package: "swift-package-manager-google-user-messaging-platform"),
+                .product(name: "MintegralAdapterTarget", package: "googleads-mobile-ios-mediation-mintegral"),
+                .product(name: "LiftoffMonetizeAdapterTarget", package: "googleads-mobile-ios-mediation-liftoffmonetize"),
+            ],
+            path: "PoingGodotAdMobDeps"
+        )
+    ]
+)
